78f137f94f232680d88b73732f131f8723198698
张恩铭 <11418411+zhangenming2821@user.noreply.gitee.com>
2026-05-22 09:11:41 +0800

    新建一个油猴脚本 让其引用这里的generals-tower-memory-marker.user.js 这样我一刷新页面 就能直接使用最新的代码

---
+++ generals-tower-memory-marker-loader.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         generals.io 塔记忆标记本地加载器
+// @namespace    https://generals.io/
+// @version      0.1.0
+// @description  引用本地 generals-tower-memory-marker.user.js，刷新页面即可使用最新代码。
+// @author       Codex
+// @match        https://generals.io/*
+// @match        http://generals.io/*
+// @match        https://ws.generals.io/*
+// @require      file:///C:/Users/Administrator/Desktop/gnrs_gpt/generals-tower-memory-marker.user.js
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
