2eafcd07e4bdf3dfb2c5cd52673cfc895b6cd565
张恩铭 <11418411+zhangenming2821@user.noreply.gitee.com>
2026-05-28 15:30:59 +0800

    显示50的倍数啊 怎么没有显示

--- 源码/功能/游戏数据进展图表.js
+++ 源码/功能/游戏数据进展图表.js
@@ -326,6 +326,10 @@ function 取得图表配置() {
       axisLabel: {
         color: 'rgba(220, 232, 248, 0.82)',
         fontWeight: 700,
+        interval(idx) {
+          const 数据点 = 数据列表[idx]
+          return 数据点?.回合 > 0 && 数据点.回合 % 50 === 0
+        },
         formatter(回合) {
           const 数值 = Number(回合)
           return 数值 > 0 && 数值 % 50 === 0 ? 回合 : ''
